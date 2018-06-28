f503881eee3ce68ca484cdde5a6f3fa4bf9a0b16
Jennifer Georgevich <jenngeorge@berkeley.edu>
2018-06-28 15:17:07 -0400

    Add null default to avro schema

--- services/spark_ingest/app/avro_schema.py
+++ services/spark_ingest/app/avro_schema.py
@@ -5,58 +5,58 @@ station_schema = """
   "name": "station_status",
   "fields": [
     {
-      "name": "stationName", "type": "string"
+      "name": "stationName", "type": ["null", "string"], "default": null
     },
     {
-      "name": "availableDocks", "type": "int"
+      "name": "availableDocks", "type": ["null", "int"], "default": null
     },
     {
-      "name": "totalDocks", "type": "int"
+      "name": "totalDocks", "type": ["null", "int"], "default": null
     },
     {
-      "name": "latitude", "type": "double"
+      "name": "latitude", "type": ["null", "double"], "default": null
     },
     {
-      "name": "longitude", "type": "double"
+      "name": "longitude", "type": ["null", "double"], "default": null
     },
     {
-      "name": "statusValue", "type": "string"
+      "name": "statusValue", "type": ["null", "string"], "default": null
     },
     {
-      "name": "statusKey", "type": "int"
+      "name": "statusKey", "type": ["null", "int"], "default": null
     },
     {
-      "name": "availableBikes", "type": "int"
+      "name": "availableBikes", "type": ["null", "int"], "default": null
     },
     {
-      "name": "stAddress1", "type": "string"
+      "name": "stAddress1", "type": ["null", "string"], "default": null
     },
     {
-      "name": "stAddress2", "type": "string"
+      "name": "stAddress2", "type": ["null", "string"], "default": null
     },
     {
-      "name": "city", "type": "string"
+      "name": "city", "type": ["null", "string"], "default": null
     },
     {
-      "name": "postalCode", "type": "string"
+      "name": "postalCode", "type": ["null", "string"], "default": null
     },
     {
-      "name": "location", "type": "string"
+      "name": "location", "type": ["null", "string"], "default": null
     },
     {
-      "name": "altitude", "type": "string"
+      "name": "altitude", "type": ["null", "string"], "default": null
     },
     {
-      "name": "testStation", "type": "boolean"
+      "name": "testStation", "type": ["null", "boolean"], "default": null
     },
     {
-      "name": "lastCommunicationTime", "type": "string"
+      "name": "lastCommunicationTime", "type": ["null", "string"], "default": null
     },
     {
-      "name": "landMark", "type": "string"
+      "name": "landMark", "type": ["null", "string"], "default": null
     },
     {
-      "name": "op", "type": "string"
+      "name": "op", "type": ["null", "string"], "default": null
     }
   ]
 }
