753a4b63bc9a85cc5fb2a398ac37462b357228d6
Jennifer Georgevich <jenngeorge@berkeley.edu>
2018-06-28 18:01:54 -0400

    Rename spark-ingest to citibike-producer, start playing with citibike consumer

--- services/citibike_producer/app/app.py
+++ services/citibike_producer/app/app.py
@@ -6,8 +6,10 @@ from pyspark import SparkContext, SparkConf
 from confluent_kafka import avro
 from confluent_kafka.avro import AvroProducer
 from avro_schema import station_schema
-#except:
-#    print('error importing for spark_ingest/app')
+
+from confluent_kafka.avro import AvroConsumer
+from confluent_kafka.avro.serializer import SerializerError
+from confluent_kafka import KafkaError
 
 
 value_schema_str = station_schema
@@ -73,7 +75,7 @@ def poll():
                             .union(rdd_deletes)
 
         for update in rdd_stream.collect():
-            print(update)
+            # print(update)
             key_dict = dict([update[0]])
             value_dict = dict(update[1])
             avroProducer.produce(topic='station_status', value=value_dict, key=key_dict)
@@ -82,5 +84,39 @@ def poll():
 
         avroProducer.flush()
 
+
+def consume():
+    c = AvroConsumer({
+    'bootstrap.servers': 'kafka-1:19092, kafka-2:29092',
+    'schema.registry.url': 'http://schema-registry:8081',
+    'group.id': 'groupid',
+    })
+
+    c.subscribe(['station_status'])
+
+    while True:
+        try:
+            msg = c.poll(10)
+
+        except SerializerError as e:
+            print("Message deserialization failed for {}: {}".format(msg, e))
+            break
+
+        if msg is None:
+            print("~~~~MESSAGE IS NONE")
+            continue
+
+        if msg.error():
+            if msg.error().code() == KafkaError._PARTITION_EOF:
+                continue
+            else:
+                print("~~A MESSAGE ERROR", msg.error())
+                break
+
+        print('~~~~~DIDD ITTTT', msg.value())
+
+    c.close()
+
 if __name__ == '__main__':
+    consume()
     poll()
